1c3ae27c3caad6a9ac1dc322353614ab9b48c415
Alefal44 <45197998+Alefal44@users.noreply.github.com>
2024-09-12 17:23:54 +0200

    test

--- DaisyProgrammer/app/app.js
+++ DaisyProgrammer/app/app.js
@@ -1,5 +1,5 @@
 // Generic Strings
-const root_url = "https://electro-smith.github.io/Programmer"
+const root_url = "https://alefal44.github.io/DaisyProgrammer"
 
 // New changes involve reading from sources.json to find the 'sources' we should pull from
 // Those sources replace the previously hard coded 'examples.json' file, and should otherwise 
